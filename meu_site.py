cde4950f07e29866fb4b4e356c88db4a2ca4817a
Matheus Mösken Diegues <69434680+dieguesmosken@users.noreply.github.com>
2023-11-27 15:12:07 -0300

    add test site

--- meu_site.py
+++ meu_site.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("index.html")
 
 @app.route("/contatos")
 def contatos():
