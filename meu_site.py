d52e68b1297084a8560baa75828e832e4b2a324f
Matheus Mösken Diegues <matheus.diegues@fatec.sp.gov.br>
2024-03-22 20:09:38 -0300

    Atualizar rotas e lógica de aplicativo

--- meu_site.py
+++ meu_site.py
@@ -1,17 +1,17 @@
 # Autor: Teuzin
-from flask import Flask, render_template,request, redirect, url_for, session
-app = Flask(__name__)
-
-# importar o banco de dados
+from flask import Flask, render_template, request, redirect, url_for, session
 from db import db, User
 
-
+# Inicializar o banco de dados
 db.create_all()
 
 app = Flask(__name__)
 app.config.from_object('config')
 db.init_app(app)
 
+# Criar um contexto de aplicativo
+with app.app_context():
+    # Suas rotas e lógica de aplicativo aqui
 
 #criar a 1ª pagina do site
 
@@ -19,61 +19,59 @@ db.init_app(app)
 
 #função -> o que voce quer exibir naquela pagina
 
-#template
+#rotas
 
-@app.route("/")
-def homepage():
-    return render_template("index.html")
+    @app.route("/")
+    def homepage():
+        return render_template("index.html")
 
-@app.route("/projetos")
-def projetos():
-    return render_template("projects.html")
+    @app.route("/projetos")
+    def projetos():
+        return render_template("projects.html")
 
-@app.route("/projetos/<projeto>")
-def projeto(projeto):
-    return render_template("projects.html", projeto=projeto)
+    @app.route("/projetos/<projeto>")
+    def projeto(projeto):
+        return render_template("projects.html", projeto=projeto)
 
-@app.route("/sobre")
-def sobre():
-    return render_template("about.html")
+    @app.route("/sobre")
+    def sobre():
+        return render_template("about.html")
 
-@app.route("/sobre/<usuario>")
-def usuario(usuario):
-    return render_template("about.html", usuario=usuario)
+    @app.route("/sobre/<usuario>")
+    def usuario(usuario):
+        return render_template("about.html", usuario=usuario)
 
-# rotas de registro e login
-@app.route("/login")
-def login():
-    return render_template("login.html")
+    # rotas de registro e login
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
 
 
-@app.route('/register', methods=['POST'])
-def register():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    @app.route('/register', methods=['POST'])
+    def register():
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
 
-    return redirect(url_for('login'))
+        return redirect(url_for('login'))
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    @app.route('/login', methods=['POST'])
+    def login():
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-    user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
-        session['logged_in'] = True
-        return redirect(url_for('home'))
+        if user and user.password == password:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
 
-    return redirect(url_for('login'))
+        return redirect(url_for('login'))
 
-#colocar o site no ar
 
+# Colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
-
-    #servidor do heroku
